c29ce9798bbf21341c641e1ef9adc29d1c6e70e9
Oskar Wiederhold <oskar.wiederhold@adnovum.ch>
2026-06-16 10:39:17 +0200

    add findByProduct to PriceEntryRepository

--- backend/src/main/java/com/motorrad/kostenmanager/repository/PriceEntryRepository.java
+++ backend/src/main/java/com/motorrad/kostenmanager/repository/PriceEntryRepository.java
@@ -1,9 +1,11 @@
 package com.motorrad.kostenmanager.repository;
-import com.motorrad.kostenmanager.model.PriceEntry;
 
+import com.motorrad.kostenmanager.model.PriceEntry;
+import com.motorrad.kostenmanager.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PriceEntryRepository extends JpaRepository<PriceEntry, Long> {
+    PriceEntry findByProduct(Product product);
 }
